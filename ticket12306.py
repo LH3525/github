66bf2e1bc98df7a02d6f2e7c49c3387b49cde437
LH3525 <1435549249@qq.com>
2018-09-28 14:59:08 +0800

    0928

--- ticket12306.py
+++ ticket12306.py
@@ -11,14 +11,15 @@ driver.find_element_by_id('toStationText').click()
 driver.find_element_by_id('toStationText').send_keys('南京\n')
 # driver.find_element_by_id('train_date')
 driver.find_element_by_id('a_search_ticket').click()
+driver.implicitly_wait(20)
 driver.find_element_by_css_selector('#date_range>ul>li:nth-child(3)').click()
 driver.implicitly_wait(30)
 sel = driver.find_element_by_id('cc_start_time')
 tmp = Select(sel)
-tmp.select_by_value('12001800')
+tmp.select_by_value('06001200')
 while(True):
     driver.find_element_by_id('query_ticket')
-    isticket = driver.find_element_by_id('ZE_5800000G560F').text
+    isticket = driver.find_element_by_id('ZE_56000G944600').text
     driver.implicitly_wait(20)
     if (isticket == '无'):
         continue
@@ -31,21 +32,17 @@ from email.header import Header
 import smtplib
 
 message ='''
-hello，world！
-来自我的电脑
+有票了!
 '''
-
 msg = MIMEText(message,'plain','utf-8')
-
 msg['Subject'] = Header("来自Python的邮件",'utf-8')
-msg['From'] = Header('XXXX@sina.com')
-msg['To'] = Header('receiver','utf-8')
+# msg['From'] = Header('XXXX@sina.com')
+# msg['To'] = Header('receiver','utf-8')
 
-from_addr = 'XXXX@sina.com' #发件邮箱
-password = 'password' #邮箱密码
-to_addr = 'XXXX@qq.com' #收件邮箱
-
-smtp_server = 'smtp.sina.com' #SMTP服务器，以新浪为例
+from_addr = '1435549249@qq.com' #发件邮箱
+password = 'odxnoxnjlipciffa' #邮箱密码
+to_addr = '1549591113@qq.com' #收件邮箱
+smtp_server = 'smtp.qq.com' #SMTP服务器，以新浪为例
 try:
     server = smtplib.SMTP(smtp_server,25) #第二个参数为默认端口为25，有些邮件有特殊端口
     print('开始登录')
