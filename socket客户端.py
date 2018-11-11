8630f870ed2faa0f5517e0d37d4f71e31ae6cee9
LH3525 <43264800+LH3525@users.noreply.github.com>
2018-11-11 10:00:35 +0800

    Update socket客户端.py

--- socket客户端.py
+++ socket客户端.py
@@ -1,11 +1,11 @@
 from socket import *
 HOST = '127.0.0.1'
-PORT = 21567
+SERVER_PORT = 21567
 BUFSIZ = 1024
-ADDR = (HOST,PORT)
+SERVER_ADDR = (HOST,SERVER_PORT)
 #指明协议
 tcpclisock = socket(AF_INET,SOCK_STREAM)#AF_INET表示IP_4协议，SOCK_STREAM表示tcp协议
-tcpclisock.connect(ADDR)
+tcpclisock.connect(SERVER_ADDR)
 while True:
     data = input('>>')
     if not data:
